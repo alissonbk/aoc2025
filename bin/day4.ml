0e8354baf500d9f2c876d599d5d8602999ffe3b5
alissonbk <alissonbk2010@hotmail.com>
2025-12-04 22:03:54 -0300

    day4 part 2

--- bin/day4.ml
+++ bin/day4.ml
@@ -25,27 +25,32 @@ let read_file =
 
 let sum_roll_element mtx py px =    
   try
-    if mtx.(py).(px) = '@' then 1 else 0
+    if mtx.(py).(px) = '@' then (
+      1
+    ) else 
+      0
   with
-    | Invalid_argument _ -> printf "a"; 0
+    | Invalid_argument _ -> 0
 
-let sum_all_suroudings mtx py px =
-  sum_roll_element mtx (py + 1) px +
+let sum_all_suroudings ?(should_change=false) mtx py px  =
+  let sum = sum_roll_element  mtx (py + 1) px +
   sum_roll_element mtx (py + 1) (px + 1) +
   sum_roll_element mtx (py + 1) (px - 1) +
   sum_roll_element mtx (py - 1) px +
   sum_roll_element mtx (py - 1) (px + 1) +
   sum_roll_element mtx (py - 1) (px - 1) +
   sum_roll_element mtx py (px + 1) +
-  sum_roll_element mtx py (px - 1)
+  sum_roll_element mtx py (px - 1) in
+  if should_change && sum < 4 then mtx.(py).(px) <- 'x';
+  sum
+  
   
 
 let puzzle1 = 
   let mtx = read_file in
   let can_be_accessed = ref 0 in
   for y = 0 to (Array.length mtx) - 1 do 
-    for x = 0 to (Array.length mtx.(y) - 1) do
-      printf "b"; 
+    for x = 0 to (Array.length mtx.(y) - 1) do      
       let c = mtx.(y).(x) in
       if c = '@' && (sum_all_suroudings mtx y x) < 4 then
         can_be_accessed := !can_be_accessed + 1
@@ -53,5 +58,23 @@ let puzzle1 =
   done;
   !can_be_accessed
 
+
+(* let print_mtx mtx =
+  mtx |> Array.iter (fun a -> a |> Array.iter (fun c -> printf "%c" c); printf "\n") *)
+
+let rec puzzle2 tot_sum = 
+  let mtx = read_file in
+  let can_be_accessed = ref 0 in  
+  for y = 0 to (Array.length mtx) - 1 do 
+    for x = 0 to (Array.length mtx.(y) - 1) do      
+      let c = mtx.(y).(x) in      
+      if c = '@' && (sum_all_suroudings ~should_change:true mtx y x) < 4 then
+        can_be_accessed := !can_be_accessed + 1
+    done
+  done;  
+  if !can_be_accessed = 0 then tot_sum else
+  puzzle2 (tot_sum + !can_be_accessed)
+
 let () = 
-  printf "puzzle1 %d\n" puzzle1
+  printf "puzzle1 %d\n" puzzle1;
+  printf "puzzle2 %d\n" (puzzle2 0)
