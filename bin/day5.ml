68972a75323b3aaf41ad50018a3458d9a1260300
alissonbk <alissonbk2010@hotmail.com>
2025-12-05 22:07:46 -0300

    day5 part 2

--- bin/day5.ml
+++ bin/day5.ml
@@ -44,42 +44,25 @@ let puzzle1 =
         
   in
   loop_ids ids 0
-
-
-(* module IntSet = Set.Make(Int)
-
-let rec add_range_to_set set curr endd =
-  if curr > endd then set else add_range_to_set (IntSet.add curr set) (curr + 1) endd *)
-
-
-(* missing check double countings *)
-let rec find_interpolations ranges curra currb sum =
-  match ranges with
-    | [] -> sum
-    | (ra, rb) :: t ->
-      let s = ref 0 in 
-      if currb > ra && currb < rb then (
-        s := !s + (currb - ra + 1)
-      );
-      if curra > ra && curra < rb then (
-        s := !s + (rb - curra + 1)
-      );
-      find_interpolations t curra currb (sum + !s)
-
-
-
-let puzzle2 =  
-  let (ranges, _) = read_file in  
-  let rec loop_ranges ranges sum =
-    match ranges with
-      | [] -> sum
-      | (curr_a, curr_b) :: t ->
-        let currdiff = (curr_b - curr_a + 1) - (find_interpolations ranges curr_a curr_b 0) in
-        loop_ranges t (sum + currdiff)
-        
-  in
-  loop_ranges ranges 0  
   
+
+let in_range v (a, b) = a <= v && v <= b
+
+let puzzle2 =
+  let (ranges, _) = read_file in
+  let sorted_ranges = List.sort (fun (a1, _) (a2, _) -> a1 - a2) ranges in
+  let rec loop ranges prev sum =
+    match ranges with
+      | [] -> let (prev_a, prev_b) = prev in sum + (prev_b - prev_a + 1)      
+      | (a, b) :: t ->
+        let (prev_a, prev_b) = prev in
+        if in_range a prev then
+          loop t ((Int.min prev_a a), (Int.max prev_b b)) sum
+        else
+          loop t (a, b) (sum + (prev_b - prev_a + 1))
+  in  
+  loop (List.tl sorted_ranges) (List.hd sorted_ranges) 0
+
 
 
 
